b03f11d9e250a2d3067deb18cf2569a04cdcf8d4
Mayank Pujara <mpujara@uclan.ac.uk>
2025-03-29 21:09:25 +0000

    fix final working tool

--- AuditRecordCounter.designer.cs
+++ AuditRecordCounter.designer.cs
@@ -32,10 +32,12 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuditRecordCounterByTable));
             this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbAudit = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbExportToExcel = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tslAbout = new System.Windows.Forms.ToolStripLabel();
             this.tsbClose = new System.Windows.Forms.ToolStripButton();
-            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.auditGridView = new System.Windows.Forms.DataGridView();
@@ -43,14 +45,10 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
-            this.tsbAudit = new System.Windows.Forms.ToolStripButton();
-            this.tsbExportToExcel = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.StatusText = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.toolStripMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.auditGridView)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStripMenu
@@ -66,16 +64,46 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
             this.toolStripMenu.Name = "toolStripMenu";
             this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
-            this.toolStripMenu.Size = new System.Drawing.Size(1025, 27);
+            this.toolStripMenu.Size = new System.Drawing.Size(1025, 31);
             this.toolStripMenu.TabIndex = 4;
             this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbAudit
+            // 
+            this.tsbAudit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbAudit.Image = ((System.Drawing.Image)(resources.GetObject("tsbAudit.Image")));
+            this.tsbAudit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAudit.Name = "tsbAudit";
+            this.tsbAudit.Size = new System.Drawing.Size(239, 28);
+            this.tsbAudit.Text = "Load Audit Record Count By Table";
+            this.tsbAudit.Click += new System.EventHandler(this.tsbAudit_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // tsbExportToExcel
+            // 
+            this.tsbExportToExcel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbExportToExcel.Image = ((System.Drawing.Image)(resources.GetObject("tsbExportToExcel.Image")));
+            this.tsbExportToExcel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExportToExcel.Name = "tsbExportToExcel";
+            this.tsbExportToExcel.Size = new System.Drawing.Size(299, 28);
+            this.tsbExportToExcel.Text = "Export Audit Record Count By Table To CSV";
+            this.tsbExportToExcel.Click += new System.EventHandler(this.tsbExportToExcel_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
             // 
             // tslAbout
             // 
             this.tslAbout.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.tslAbout.IsLink = true;
             this.tslAbout.Name = "tslAbout";
-            this.tslAbout.Size = new System.Drawing.Size(88, 24);
+            this.tslAbout.Size = new System.Drawing.Size(88, 28);
             this.tslAbout.Text = "by MayankP";
             this.tslAbout.Click += new System.EventHandler(this.tslAbout_Click);
             // 
@@ -84,23 +112,9 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.tsbClose.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.tsbClose.Name = "tsbClose";
-            this.tsbClose.Size = new System.Drawing.Size(107, 24);
+            this.tsbClose.Size = new System.Drawing.Size(107, 28);
             this.tsbClose.Text = "Close this tool";
             this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
-            // 
-            // tssSeparator1
-            // 
-            this.tssSeparator1.Name = "tssSeparator1";
-            this.tssSeparator1.Size = new System.Drawing.Size(6, 27);
-            // 
-            // progressBar
-            // 
-            this.progressBar.Location = new System.Drawing.Point(0, 8);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(1025, 36);
-            this.progressBar.TabIndex = 26;
-            this.progressBar.Visible = false;
             // 
             // contextMenuStrip1
             // 
@@ -114,13 +128,13 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.auditGridView.AllowUserToDeleteRows = false;
             this.auditGridView.AllowUserToOrderColumns = true;
             this.auditGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.auditGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.auditGridView.Location = new System.Drawing.Point(0, 152);
+            this.auditGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.auditGridView.Location = new System.Drawing.Point(0, 31);
             this.auditGridView.Name = "auditGridView";
             this.auditGridView.ReadOnly = true;
             this.auditGridView.RowHeadersWidth = 51;
             this.auditGridView.RowTemplate.Height = 24;
-            this.auditGridView.Size = new System.Drawing.Size(1025, 529);
+            this.auditGridView.Size = new System.Drawing.Size(1025, 650);
             this.auditGridView.TabIndex = 38;
             // 
             // groupBox2
@@ -131,35 +145,19 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.groupBox2.TabIndex = 40;
             this.groupBox2.TabStop = false;
             // 
-            // tsbAudit
+            // progressBar
             // 
-            this.tsbAudit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.tsbAudit.Image = ((System.Drawing.Image)(resources.GetObject("tsbAudit.Image")));
-            this.tsbAudit.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbAudit.Name = "tsbAudit";
-            this.tsbAudit.Size = new System.Drawing.Size(239, 24);
-            this.tsbAudit.Text = "Load Audit Record Count By Table";
-            this.tsbAudit.Click += new System.EventHandler(this.tsbAudit_Click);
-            // 
-            // tsbExportToExcel
-            // 
-            this.tsbExportToExcel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.tsbExportToExcel.Image = ((System.Drawing.Image)(resources.GetObject("tsbExportToExcel.Image")));
-            this.tsbExportToExcel.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbExportToExcel.Name = "tsbExportToExcel";
-            this.tsbExportToExcel.Size = new System.Drawing.Size(299, 24);
-            this.tsbExportToExcel.Text = "Export Audit Record Count By Table To CSV";
-            this.tsbExportToExcel.Click += new System.EventHandler(this.tsbExportToExcel_Click);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            this.progressBar.Location = new System.Drawing.Point(0, 8);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1025, 36);
+            this.progressBar.TabIndex = 26;
+            this.progressBar.Visible = false;
             // 
             // StatusText
             // 
             this.StatusText.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.StatusText.Location = new System.Drawing.Point(3, 52);
+            this.StatusText.Location = new System.Drawing.Point(3, 74);
             this.StatusText.Margin = new System.Windows.Forms.Padding(4);
             this.StatusText.Multiline = true;
             this.StatusText.Name = "StatusText";
@@ -168,24 +166,12 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.StatusText.Size = new System.Drawing.Size(1019, 42);
             this.StatusText.TabIndex = 27;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.StatusText);
-            this.groupBox1.Controls.Add(this.progressBar);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Location = new System.Drawing.Point(0, 27);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1025, 97);
-            this.groupBox1.TabIndex = 39;
-            this.groupBox1.TabStop = false;
-            // 
             // AuditRecordCounterByTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.auditGridView);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.toolStripMenu);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AuditRecordCounterByTable";
@@ -195,8 +181,6 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
             this.toolStripMenu.ResumeLayout(false);
             this.toolStripMenu.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.auditGridView)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,7 +191,6 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
         private System.Windows.Forms.ToolStrip toolStripMenu;
         private System.Windows.Forms.ToolStripButton tsbClose;
         private System.Windows.Forms.ToolStripSeparator tssSeparator1;
-        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.ToolStripLabel tslAbout;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
@@ -219,7 +202,8 @@ namespace Sdmsols.XTB.AuditRecordCounterByTable
         private System.Windows.Forms.ToolStripButton tsbAudit;
         private System.Windows.Forms.ToolStripButton tsbExportToExcel;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.TextBox StatusText;
-        private System.Windows.Forms.GroupBox groupBox1;
+       
     }
 }
